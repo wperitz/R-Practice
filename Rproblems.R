a0be9808b4dd5ef154b60260c709b31e8b882bd2
wperitz <wgp3aq@virginia.edu>
2024-06-14 15:15:03 -0400

    added questions 1 and 2

--- Rproblems.R
+++ Rproblems.R
@@ -1,1 +1,55 @@
-1 + 2
+library(tidyverse)
+
+# Problem 1
+
+sum1 = 0
+n = 1
+
+while (n < 1000) {
+  if (n %% 3 == 0 || n %% 5 == 0) {
+    sum1 = sum1 + n
+  }
+  n = n + 1
+}
+
+print(sum1)
+
+# Problem 2
+
+x = 1
+y = 2
+sum2 = 0
+
+while (y <= 4000000){
+  if (y %% 2 == 0){
+    sum2 = sum2 + y
+  }
+  z = y
+  y = x + y
+  x = z
+  
+}
+
+print(sum2)
+
+
+# Problem 3
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
